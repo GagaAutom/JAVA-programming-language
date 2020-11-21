88cd13b8d3dce45d97952b99141b3814e15cb049
GagaAutom <1049340665@qq.com>
2020-11-21 19:46:03 +0800

    引用类型使用

---
+++ 20201121/StringL.java
@@ -0,0 +1,17 @@
+public class StringL{
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s);
+
+        String a = "hello";
+        String b = "world";
+        String c = a + b;
+        System.out.println(c);
+
+        String str = "result =";
+        int i = 10;
+        int j = 20;
+        String result = str + i + j;
+        System.out.println(result);
+    }
+}
