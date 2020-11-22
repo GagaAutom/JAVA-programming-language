ed1b928b0d16439edf3eb483ca1fc04ffb417fe8
GagaAutom <1049340665@qq.com>
2020-11-22 19:27:13 +0800

    熟悉运算符

---
+++ 20201122/Test.java
@@ -0,0 +1,25 @@
+public class Test{
+
+    public static void main(String[] args) {
+        int a = 10; 
+        int b = 20; 
+        System.out.println(a == b); 
+        System.out.println(a != b); 
+        System.out.println(a < b); 
+        System.out.println(a > b); 
+        System.out.println(a <= b); 
+        System.out.println(a >= b); 
+    }
+    public static void main2(String[] args) {
+        System.out.println(10%3);
+        System.out.println(-10%3);
+        System.out.println(10%-3);
+        System.out.println(-10%-3);
+    }
+    public static void main1(String[] args) {
+        System.out.println(5/2);
+        System.out.println(5.0/2);
+        System.out.println((float)5/2);
+        System.out.println((float)(5/2));//2.0
+    }
+}
